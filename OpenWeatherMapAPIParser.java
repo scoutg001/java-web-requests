62539b5701ae242f85fac02e6136489317bae523
scoutg <37025120+scoutg001@users.noreply.github.com>
2025-03-20 18:07:28 -0400

    thread-safe static refactor

--- OpenWeatherMapAPIParser.java
+++ OpenWeatherMapAPIParser.java
@@ -1,32 +1,25 @@
 import org.json.JSONObject;
 import org.json.JSONArray;
 
-public class OpenWeatherMapAPIParser implements Weather{
-    private static double tempurature;
-    private static String condition;
-    //private static final TempuratureUnit ORIGINAL_UNIT=TempuratureUnit.KELVIN;
-
-    private static void fromJSON(String json){
+public class OpenWeatherMapAPIParser{
+    public static double getTempurature(String json, TempuratureUnit unit){
         JSONObject rootData=new JSONObject(json);
         JSONObject main=rootData.getJSONObject("main");
-        OpenWeatherMapAPIParser.tempurature=main.getFloat("temp");
-        JSONArray weather=rootData.getJSONArray("weather");
-        OpenWeatherMapAPIParser.condition=weather.getJSONObject(0).getString("main");
-    }
+        double tempurature=main.getFloat("temp");
 
-    public static double getTempurature(String json, TempuratureUnit unit){
-        fromJSON(json);
         if(unit.equals(TempuratureUnit.CELSIUS)){
-            return OpenWeatherMapAPIParser.tempurature-273.15;
+            return tempurature-273.15;
         }
         if(unit.equals(TempuratureUnit.FAHRENHEIT)){
-            return ((OpenWeatherMapAPIParser.tempurature-273.15)*(9.0/5.0))+32;
+            return ((tempurature-273.15)*(9.0/5.0))+32;
         }
-        return OpenWeatherMapAPIParser.tempurature;
+        return tempurature;
     }
 
     public static String getCondition(String json){
-        fromJSON(json);
-        return OpenWeatherMapAPIParser.condition;
+        JSONObject rootData=new JSONObject(json);
+        JSONArray weather=rootData.getJSONArray("weather");
+        String condition=weather.getJSONObject(0).getString("main");
+        return condition;
     }
 }
--- Weather.java
+++
@@ -1,9 +0,0 @@
-public interface Weather {
-    private static void fromJSON(String json){}
-    public static double getTempurature(String json, TempuratureUnit unit){
-        return -1;
-    }
-    public static String getCondition(String json){
-        return null;
-    }
-}
