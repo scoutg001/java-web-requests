f8f79f0ad3c0470ba900a19e14d740f2a1033e11
scoutg <37025120+scoutg001@users.noreply.github.com>
2025-03-21 00:15:18 -0400

    better handling for json errors

--- OpenWeatherMapAPIParser.java
+++ OpenWeatherMapAPIParser.java
@@ -1,10 +1,14 @@
 import org.json.JSONObject;
+import org.json.JSONArray;
 
 public class OpenWeatherMapAPIParser{
     public static double getTemperature(String json, TemperatureUnit unit){
         JSONObject rootData=new JSONObject(json);
-        JSONObject main=rootData.getJSONObject("main");
-        double temperature=main.getFloat("temp");
+        JSONObject main=rootData.optJSONObject("main"); //optJSONObject doesn't error out if the main JSON key doesn't exist due to an API change
+        if(main==null){
+            System.err.println("JSON String is missing 'main' object.");
+        }
+        double temperature=main.optDouble("temp", 273.15); //returns 0 degrees celsius by default
 
         switch(unit){
             case CELSIUS:
@@ -17,9 +21,12 @@ public class OpenWeatherMapAPIParser{
     }
 
     public static String getCondition(String json){
-        return new JSONObject(json)
-            .getJSONArray("weather")
-            .getJSONObject(0)
-            .getString("main");
+        JSONArray weather=new JSONObject(json)
+            .optJSONArray("weather"); //optJSONArray doesn't error out if the weather JSON array key doesn't exist due to an API change
+        if(weather==null){
+            return "Unknown";
+        }
+        return weather.getJSONObject(0)
+            .optString("main", "Unknown");
     }
 }
