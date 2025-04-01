8e24d7ca31585ead7aa3f770a56da510c34263f8
scoutg <37025120+scoutg001@users.noreply.github.com>
2025-04-01 17:16:19 -0400

    made a cache object

---
+++ CacheObject.java
@@ -0,0 +1,20 @@
+public class CacheObject<T> {
+    private T value;
+    private long timestamp;
+
+    public CacheObject(T value, long timestamp){
+        this.value=value;this.timestamp=timestamp;
+    }
+
+    public T getValue(){return this.value;}
+    public long getTimestamp(){return this.timestamp;}
+
+    public boolean isValid(long cacheDuration){
+        long currentTime=System.currentTimeMillis();
+        return currentTime-this.timestamp <=cacheDuration;
+    }
+
+    public boolean isExpired(long cacheDuration){
+        return !isValid(cacheDuration);
+    }
+}
